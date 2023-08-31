108ad606a07dcf0820e57be1b65dbad62cc79a68
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-08-31 22:32:19 +0600

    add navbar and

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 console.log('hello world');
+
+const handelCategory = async()=>{
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
+    const data = await response.json();
+    const tapData = data.data
+   // console.log(tapData);
+    const tapContainer = document.getElementById('tap-container')
+    tapData.forEach(item => {
+       // console.log(item);
+        const createDiv =document.createElement('div')
+        createDiv.innerHTML=`
+        <a onclick = " displayLoad('${item.category_id}')" class="tab text-2xl text-black"">${item.category}</a> 
+        `
+
+        tapContainer.appendChild(createDiv)
+    });
+}
+
+const displayLoad = async (itemId) =>{
+    const response = await fetch (`https://openapi.programming-hero.com/api/videos/category/${itemId}`);
+    const data = await response.json();
+    const cardItems = data.data
+    console.log(cardItems);
+    const cardContainer = document.getElementById('card-container');
+
+    cardItems.forEach = (item) =>{
+        console.log(item);
+
+    }
+
+
+}
+handelCategory()
