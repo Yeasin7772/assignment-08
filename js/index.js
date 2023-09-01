368ce828473e2951338d241e11a373d74c324731
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-09-01 18:00:36 +0600

    drawing tab done

--- js/index.js
+++ js/index.js
@@ -20,51 +20,68 @@ const handelCategory = async () => {
 const displayLoad = async (itemId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${itemId}`);
     const data = await response.json();
-    const cardItems = data.data
-    console.log(cardItems);
-    const cardContainer = document.getElementById('card-container');
-    cardContainer.innerHTML=''
 
-    cardItems.forEach(news => {
-        console.log(news);
-        const div = document.createElement('div');
+    const cardItems = data.data
+    // const array = cardItems.length
+    console.log(cardItems);
+    //const array = cardItems.length
+
+    if (cardItems.length ===0) {
+        const drawing = document.getElementById('drawing-container')
+        
+       
+       
+        const div = document.createElement('div')
 
         div.innerHTML = `
-   
+        <img class="" src="./images/Icon.png" alt="">
+        <h1 class="text-2xl font-semibold">Oops!! Sorry, There is no content here</h1>
+        `
+        drawing.appendChild(div)
+
+    } else {
+        
+    }
 
 
-    <div class="card lg:w-auto bg-base-100 shadow-xl">
-  <figure><img class="h-52" src="${news?.thumbnail? news.thumbnail : ' <img src="./images/Icon.png" alt="">'} " alt="Shoes" /></figure>
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = ''
+    cardItems.forEach(news => {
+        //console.log(news);
+        const div = document.createElement('div');
+        div.innerHTML = `
 
-  <div class="flex gap-4 mt-4">
-  <div>
-      <div class="avatar online">
-          <div class="w-14 rounded-full">
-              <img
-                  src="${news?.authors[0]?.profile_picture}" />
-          </div>
-      </div>
-  </div>
-  <div>
-  <h2 class="card-title text-xl">${news?.title}</h2>
-      
-  </div>
-</div>
-  <div class="card-body">
-    <h2 class="card-title text-xl">${news?.authors[0]?.profile_name}  
-    <img src="${news.authors[0].verified? `svg.svg` : ''}" alt="">
-    </h2>
-    <p>${news?.others?.views}  views</p>
-    
-  </div>
-</div>
-
-    
-    `
-
-
+        <div class="card lg:w-auto bg-base-100 shadow-xl">
+        <figure><img class="h-52" src="${news?.thumbnail ? news.thumbnail : ' <img src="./images/Icon.png" alt="">'} " alt="Shoes" /></figure>
+       
+        <div class="flex gap-4 mt-4">
+        <div>
+            <div class="avatar online">
+                <div class="w-14 rounded-full">
+                    <img
+                        src="${news?.authors[0]?.profile_picture}" />
+                </div>
+            </div>
+        </div>
+        <div>
+        <h2 class="card-title text-xl">${news?.title}</h2>
+            
+        </div>
+       </div>
+        <div class="card-body">
+          <h2 class="card-title text-xl">${news?.authors[0]?.profile_name}  
+          <img src="${news.authors[0].verified ? `svg.svg` : ''}" alt="">
+          </h2>
+          <p>${news?.others?.views}  views</p>
+          
+        </div>
+       </div>
+       
+          
+          `
         cardContainer.appendChild(div)
     })
+
 
 
 }
